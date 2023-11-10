c2e76eb6be0b9df3cb2ee77ccbe83bfce517392d
KiwiHour <matthew.teabear.basford@gmail.com>
2023-11-10 02:20:23 +0000

    nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,34 @@
 
 	// let dummyData = data.dummyData;
 	let displayJumpscare = false;
-
-	onMount(() => setTimeout(() => displayJumpscare = true, 2500))
+	let playJumpscare = false;
+	let huhWeird = false;
 
 </script>
 
 <main>
 
-	:)
 	<!-- {#each dummyData as onePieceOfInformation}
 		<div class="one-piece">
 			{JSON.stringify(onePieceOfInformation)}
 		</div>
 	{/each} -->
 
+	<input type="button" value="click me" on:click={() => {huhWeird = true; setTimeout(() => {displayJumpscare=true;playJumpscare=true;huhWeird=false}, Math.random()*4000+3000)}}>
+
+	{#if huhWeird}
+		huh... weird. nothing happened
+	{/if}
+
 	{#if displayJumpscare}
 		<img src="https://media.tenor.com/u4x8IHSNnqkAAAAi/fnaf-jump-scare.gif" alt="jumpscare... bitch!" />
-		<audio autoplay src="/jumpscare.mp3"></audio>
-		{setTimeout(() => displayJumpscare = false, 475).toString().slice(0,0)}
+		{setTimeout(() => displayJumpscare = false, 875).toString().slice(0,0)}
+		{setTimeout(() => playJumpscare = false, 1000).toString().slice(0,0)}
+	{/if}
+
+	{#if playJumpscare}
+		<audio autoplay loop  id="playAudio">
+			<source src="/jumpscare.mp3">
+		</audio>
 	{/if}
 </main>
